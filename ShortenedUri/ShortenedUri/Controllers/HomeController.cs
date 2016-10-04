c9909a717fd43a80f6aba2cd53830bcb00df55ea
thanh tran huy <thanh tran huy>
2016-10-05 00:23:22 +1100

    Initial project and information

---
+++ ShortenedUri/ShortenedUri/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShortenedUri.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Shortened Uri Service";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "My Contact";
+
+            return View();
+        }
+    }
+}
