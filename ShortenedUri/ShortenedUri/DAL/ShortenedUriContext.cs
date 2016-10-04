37e463ead4abb91e6e17a026c4cd5319f2abe51b
thanh tran huy <thanh tran huy>
2016-10-05 00:34:49 +1100

    Setup entity and connection string

---
+++ ShortenedUri/ShortenedUri/DAL/ShortenedUriContext.cs
@@ -0,0 +1,22 @@
+﻿using ShortenedUri.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ShortenedUri.DAL
+{
+    public class ShortenedUriContext : DbContext
+    {
+        public ShortenedUriContext() : base("ShortenedUriContext")
+        {
+        }
+
+        public DbSet<ShortenedUrlModel> ShortenedUrls { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<ShortenedUrlModel>().ToTable("ShortenedUrl");
+        }
+    }
+}
---
+++ ShortenedUri/ShortenedUri/Models/ShortenedUrlModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+    
+namespace ShortenedUri.Models
+{
+    public class ShortenedUrlModel
+    {
+        public int Id { get; set; }
+        public string ShortUrl { get; set; }
+        public string LongUrl { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
